51d151e2131fcf3745604cb9200b31275d4f20f1
Parth Dhavan <parthdhavan2004@gmail.com>
2024-07-04 20:44:30 +0530

    Fixes #6:SetUp Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './components/Home/HomePage';
 import Generate from './components/Generate/generate'; // Ensure this path is correct
+import Login from './components/Login/login'; // Import your Login component
 import Navbar from './components/Nav/Navbar';
 import './App.css';
 
 function App() {
+  const location = useLocation();
+  const hideNavbarRoutes = ['/login'];
+
   return (
     <div className="App">
-      <Navbar/>
+      {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/generate" element={<Generate />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
