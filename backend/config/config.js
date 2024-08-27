a10040a58262707ecbcf75754c64c58a56886a55
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2024-08-27 23:42:04 +0530

    --

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 module.exports = {
-  TENSORART_APP_ID: "soTDYEsuj",
-  TENSORART_MODEL_ID: "677504969223601395",
-  TENSORART_TOKEN: "ce40e4681a854c2fa6ed5dedbc4e585c",
-  TENSORART_API_ENDPOINT: "ap-east-1.tensorart.cloud"
+  TENSORART_APP_ID: "",
+  TENSORART_MODEL_ID: "",
+  TENSORART_TOKEN: "",
+  TENSORART_API_ENDPOINT: ""
 };
